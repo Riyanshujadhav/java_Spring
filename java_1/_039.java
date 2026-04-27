3b6b9cc84dd039af974485cfbcf766dd6c739cc2
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-27 19:40:41 +0530

    javaCore

--- java_1/_039.java
+++ java_1/_039.java
@@ -21,9 +21,10 @@ public class _039 {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                s[i][j] = a[i][j]+b[i][j];
+                s[i][j] = a[i][j]*b[i][j];
             }
         }
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.print(s[i][j]+",");
