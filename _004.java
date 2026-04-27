52b7b02565cda09b1182942c9ad29800186188c8
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-27 18:02:52 +0530

    javaCore

--- _004.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class _004 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        char ch=sc.next().charAt(0);
-        System.out.println((int)ch);
-    }
-}
