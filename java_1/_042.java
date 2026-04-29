64c162c673ba0e5909200d0f6662bf6d4224e62c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-29 22:54:40 +0530

    coreJava

---
+++ java_1/_042.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class _042 {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int student=sc.nextInt();
+        int [][] marks =new int[student][];
+        for(int i=0;i<student;i++){
+            int subject=sc.nextInt();
+            marks[i]=new int[subject];
+            for(int j=0;j<subject;j++){
+                marks[i][j]=sc.nextInt();
+            }
+        }
+        for (int i = 0; i < student; i++) {
+            System.out.print("Student " + (i + 1) + ": ");
+            for (int j = 0; j < marks[i].length; j++) {
+                System.out.print(marks[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
