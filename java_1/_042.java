60284e17da7c00b300e56b7646511be7bf53159c
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-04-30 22:58:15 +0530

    javacore

--- java_1/_042.java
+++ java_1/_042.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class _042 {
-        public static void main(String[] args) {
+            public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int student=sc.nextInt();
         int [][] marks =new int[student][];
